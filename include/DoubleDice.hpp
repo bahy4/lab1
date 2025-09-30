ac36b2bf468eda9370d7ff8a65c681e7298a24d8
Andrey <andrey-bakhvalov@mail.ru>
2025-09-30 17:36:48 +0000

    alternative double dice added

--- include/DoubleDice.hpp
+++ include/DoubleDice.hpp
@@ -4,11 +4,23 @@
 #include "Rollable.hpp"
 #include <memory>
 
+/*//DoubleDice без наследования
+class DoubleDice : public Rollable{
+public:
+    DoubleDice(std::unique_ptr<Rollable> dice);
+    unsigned roll() override;
+    ~DoubleDice() override;
+private:
+    std::unique_ptr<Rollable> baseDice;
+};*/
+
+//DoubleDice с наследованием
 class DoubleDice: public BonusDice, public PenaltyDice{
 public:
     DoubleDice(std::unique_ptr<Rollable> dice);
     unsigned roll() override;
     DoubleDice() : PenaltyDice(), BonusDice() {}
     ~DoubleDice() override;
-    
+private:
+    std::unique_ptr<Rollable> baseDice;
 }; 
--- src/DoubleDice.cpp
+++ src/DoubleDice.cpp
@@ -5,10 +5,38 @@
 #include "Rollable.hpp"
 #include <memory>
 #include <utility>
+#include <cmath>
 
-DoubleDice::DoubleDice(std::unique_ptr<Rollable> dice){
-    PenaltyDice::baseDice = std::make_unique<Dice>(*dynamic_cast<Dice*>(dice.get()));
-    BonusDice::baseDice = std::move(dice);
+/*//DoubleDice без наследования
+DoubleDice::DoubleDice (std::unique_ptr<Rollable> dice)
+    : baseDice(std::move(dice)){}
+
+DoubleDice::~DoubleDice()=default;
+
+unsigned DoubleDice::roll(){
+    unsigned roll1 = baseDice->roll();
+    unsigned roll2 = baseDice->roll();
+    unsigned roll3 = baseDice->roll();
+    unsigned roll4 = baseDice->roll();
+    
+    return fmax(roll1, roll2) + fmin(roll3, roll4);
+}*/
+
+
+//DoubleDice с наследованием
+class SharedDiceWrapper : public Rollable {
+public:
+    SharedDiceWrapper(Rollable& dice) : refDice(dice) {}
+    unsigned roll() override { return refDice.roll(); }
+private:
+    Rollable& refDice;
+};
+
+DoubleDice::DoubleDice(std::unique_ptr<Rollable> dice) 
+    : PenaltyDice(nullptr), BonusDice(nullptr), baseDice(std::move(dice)) {
+    
+    PenaltyDice::baseDice = std::make_unique<SharedDiceWrapper>(*baseDice);
+    BonusDice::baseDice = std::make_unique<SharedDiceWrapper>(*baseDice);
 }
 
 unsigned DoubleDice::roll(){
